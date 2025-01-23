84306686424ebad20bbf748b1d3d3d0327c925e3
Nityam Mishra <nityam1111@gmail.com>
2025-01-23 06:42:56 +0530

    Resolved merge conflicts

--- src/RTK/Slices/paymentSlice.js
+++ src/RTK/Slices/paymentSlice.js
@@ -1,7 +1,7 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:8000/api'; // Updated to your server's base URL
+const API_BASE_URL = 'https://lotugreenmanagementbackend.onrender.com/api'; // Updated to your server's base URL
 
 // Thunks
 
